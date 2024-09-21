123f2edb3f9b3b85cf7d5b774ad2768ca38a540f
Prakul2006 <venkatdri74@gmail.com>
2024-09-22 02:20:19 +0530

    added plus in connect and integrated mailto:

--- idea2prof/script.js
+++ idea2prof/script.js
@@ -111,9 +111,11 @@ function requestProf() {
                     </div>
                     <div class="card-connect">
                         <!-- connect button -->
-                        <button class="connect-btn" onclick="connect(${prof.username})">
-                            Connect
-                            <span id="connect-btn-icon"></span>
+                        <button class="connect-btn" onclick="connect('${prof.name}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                        </svg>
+                        Connect
                         </button>
                     </div>
                     <div class="card-lastup">
@@ -157,4 +159,10 @@ function clear_page() {
     });
     domainList = [];
     searchBar.value = "";
+}
+function connect(name) {
+    name = name.replace(/\s/g, "");
+    console.log(name);
+    var win = window.open('mailto:'+name.toLowerCase()+'@vit.ac.in?subject=Research Collaboration Inquiry', '_blank');
+    win.focus();
 }
